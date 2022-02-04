e506435c9a93f5dcff93a64fcc201b768abc03d6
Lewis Hardeman <hardeman.lewis@gmail.com>
2022-02-04 16:40:06 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
@@ -8,46 +8,25 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/* <Counter /> */}
+        
+        <div className="Coming-soon">
+          <h1>COMING SOON!</h1>
+
+          <p>
+          </p>
+
+        </div>
+
+
         <span>
-          <span>Learn </span>
           <a
             className="App-link"
-            href="https://reactjs.org/"
+            href="mailto:hardeman.lewis@gmail.com"
             target="_blank"
             rel="noopener noreferrer"
           >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
+            Contact Us
           </a>
         </span>
       </header>
