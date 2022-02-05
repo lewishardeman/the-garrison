21438db357d5196c6cdc28a9806087ad538cd38a
Lewis Hardeman <hardeman.lewis@gmail.com>
2022-02-04 19:04:55 -0500

    Landing Page

--- src/App.js
+++ src/App.js
@@ -11,23 +11,44 @@ function App() {
         {/* <Counter /> */}
         
         <div className="Coming-soon">
-          <h1>COMING SOON!</h1>
-
-          <p>
-          </p>
-
+          <h1>COMING SOON</h1>
+          <h3>A community for Veterans, First Responders, and Patriots</h3>
         </div>
 
 
-        <span>
-          <a
-            className="App-link"
-            href="mailto:hardeman.lewis@gmail.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Contact Us
-          </a>
+        <span className="temp-home-links">
+          <h3>
+
+
+            <a
+              className="App-link"
+              href="mailto:hardeman.lewis@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              INVEST
+            </a>
+
+            <a
+              className="App-link"
+              href="mailto:hardeman.lewis@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              DONATE
+            </a>
+
+            <a
+              className="App-link"
+              href="mailto:hardeman.lewis@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              LEARN MORE
+            </a>
+
+          </h3>
+
         </span>
       </header>
     </div>
